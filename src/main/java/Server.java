9d37ae31e4fe7832abfcb4f23d5b1027feade142
Maksim <madmaxim22@gmail.com>
2023-03-06 20:45:48 +0300

    refactor: change "start" method in Server

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -7,7 +7,9 @@ import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -21,9 +23,11 @@ public class Server {
     private int port;
     private final ServerSocket serverSocket;
     private final ExecutorService service;
+    Map<Map<HTTPMethod, String>, Handler> handlerMap;
 
     public Server(int port) throws IOException {
         this.port = port;
+        this.handlerMap = new HashMap<>();
         this.serverSocket = new ServerSocket(port);
         this.service = Executors.newFixedThreadPool(64);
     }
@@ -33,7 +37,33 @@ public class Server {
             try {
                 Socket clientSocket = serverSocket.accept();
                 service.submit(() -> {
-                    handler(clientSocket);
+                    try {
+                        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                        StringBuilder builder = new StringBuilder();
+                        while (in.ready()) {
+                            builder.append(in.readLine());
+                            builder.append("\r\n");
+                        }
+                        Request request = new Request(builder.toString());
+                        for (var letterEntry : handlerMap.entrySet()) {
+                            Handler handler = letterEntry.getValue();
+                            for (var Entry : letterEntry.getKey().entrySet()) {
+                                HTTPMethod httpMethod = Entry.getKey();
+                                String message = Entry.getValue();
+                                if (httpMethod.equals(HTTPMethod.GET)){
+                                    if (message.equals(request.getUrl())) {
+                                        Map<HTTPMethod, String> map = new HashMap<>();
+                                        map.put(httpMethod, message);
+                                        handler = handlerMap.get(map);
+                                        handler.handle(request, new BufferedOutputStream(clientSocket.getOutputStream()));
+                                    }
+                                }
+                            }
+                        }
+
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
                 });
             } catch (IOException e) {
                 e.printStackTrace();
