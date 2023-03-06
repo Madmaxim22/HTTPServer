60558263340370063606de7139c9c433e14f7ebd
Maksim <madmaxim22@gmail.com>
2023-03-06 20:18:05 +0300

    feat: implement start server

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,2 +1,30 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+
 public class Server {
+    final List<String> validPaths =
+            List.of("/index.html", "/spring.svg", "/spring.png",
+                    "/resources.html", "/styles.css", "/app.js",
+                    "/links.html", "/forms.html", "/classic.html",
+                    "/events.html", "/events.js");
+
+    private int port;
+    private final ServerSocket serverSocket;
+
+    public Server(int port) throws IOException {
+        this.port = port;
+        this.serverSocket = new ServerSocket(port);
+    }
+
+    public void start() {
+        while (true) {
+            try {
+                Socket clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
