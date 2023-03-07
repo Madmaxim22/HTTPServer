62cf998bda14cefd530d22c58f461c52e1980c49
Maksim <madmaxim22@gmail.com>
2023-03-07 18:45:57 +0300

    refactor: handle

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,28 @@
 import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Server server = new Server(9999);
 
-        server.addHandler(HTTPMethod.GET, "/index.html", new Handler() {
+        server.addHandler(HTTPMethod.GET, "/public", new Handler() {
             public void handle(Request request, BufferedOutputStream responseStream) {
                 try(final BufferedOutputStream out = responseStream) {
                     final String path = request.getUrl();
                     Response response = new Response();
-                    response.setBodyInFile(path);
+                    File f = new File("." + path);
+                    if(f.exists() && !f.isDirectory()) {
+                        response.setBodyInFile(path);
+                    } else {
+                        response.setStatusCode(404);
+                        response.setStatus("Not found");
+                        response.setBodyInFile("/public/404.html");
+                    }
                     out.write(response.getByte());
                     out.flush();
                 } catch (IOException e) {
-                    throw new RuntimeException(e);
+                    e.printStackTrace();
                 }
             }
         });
